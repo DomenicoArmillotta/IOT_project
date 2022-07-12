5fb7b7f58d0e3d50debce867d1cd5636595f4c6c
Domenico <10582521@polimi.it>
2022-07-12 20:55:18 +0200

    update directory

---
+++ sensors/coap_sensor_alert/alert.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "contiki.h"
+#include "sys/etimer.h"
+//#include "dev/leds.h"
+
+#include "node-id.h"
+#include "net/ipv6/simple-udp.h"
+#include "net/ipv6/uip.h"
+#include "os/dev/leds.h"
+#include "net/ipv6/uip-ds6.h"
+#include "os/dev/button-hal.h"
+#include "net/ipv6/uip-debug.h"
+#include "routing/routing.h"
+
+#include "coap-engine.h"
+#include "coap-blocking-api.h"
+
+#define SERVER_EP "coap://[fd00::1]:5683"
+#define CONN_TRY_INTERVAL 1
+#define REG_TRY_INTERVAL 1
+#define SENSOR_TYPE "alert_actuator"
+#define SIMULATION_INTERVAL 30
+#define TIMEOUT_INTERVAL 30
+
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_APP
+
+/* RESOURCES */
+double intensity = 5.0;
+
+PROCESS(alert_server, "Server for the alert actuator");
+AUTOSTART_PROCESSES(&alert_server);
+
+//*************************** GLOBAL VARIABLES *****************************//
+char* service_url = "/registration";
+
+static bool connected = false;
+static bool registered = false;
+bool pressed = false;
+
+
+static struct etimer wait_connectivity;
+static struct etimer wait_registration;
+static struct etimer simulation;
+static struct etimer timeout_timer;
+
+
+extern coap_resource_t alert_actuator;
+extern coap_resource_t  alert_switch_actuator;
+
+//*************************** UTILITY FUNCTIONS *****************************//
+static void check_connection()
+{
+    if (!NETSTACK_ROUTING.node_is_reachable())
+    {
+        LOG_INFO("BR not reachable\n");
+        etimer_reset(&wait_connectivity);
+    }
+    else
+    {
+        LOG_INFO("BR reachable");
+        // TODO: notificare in qualche modo che si è connessi
+        // gli altri hanno usato i led
+        connected = true;
+    }
+}
+
+void client_chunk_handler(coap_message_t *response)
+{
+    const uint8_t* chunk;
+
+    if (response == NULL)
+    {
+        LOG_INFO("Request timed out\n");
+        etimer_set(&wait_registration, CLOCK_SECOND * REG_TRY_INTERVAL);
+        return;
+    }
+
+    int len = coap_get_payload(response, &chunk);
+
+    if(strncmp((char*)chunk, "Success", len) == 0)
+        registered = true;
+    else
+        etimer_set(&wait_registration, CLOCK_SECOND * REG_TRY_INTERVAL);
+}
+
+
+
+//*************************** THREAD *****************************//
+PROCESS_THREAD(alert_server, ev, data)
+{
+    button_hal_button_t *btn;
+
+   static struct etimer et;
+
+   PROCESS_BEGIN();
+   etimer_set(&et, 2*CLOCK_SECOND);
+
+    btn = button_hal_get_by_index(0);
+
+
+    static coap_endpoint_t server_ep;
+    static coap_message_t request[1]; // This way the packet can be treated as pointer as usual
+
+    etimer_set(&wait_connectivity, CLOCK_SECOND * CONN_TRY_INTERVAL);
+
+    while (!connected) {
+        PROCESS_WAIT_UNTIL(etimer_expired(&wait_connectivity));
+        check_connection();
+        }
+        LOG_INFO("CONNECTED\n");
+
+    while (!registered) {
+        LOG_INFO("Sending registration message\n");
+
+        coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+
+        coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+        coap_set_header_uri_path(request, service_url);
+        //nel payload abbiamo solo il tipo di sensore = alert_actuator
+        char msg[300];
+        char* sensor_type = "";
+        sprintf(sensor_type, "{\"Resource\":\"%s}", SENSOR_TYPE);
+        strcpy(msg, sensor_type);
+        coap_set_payload(request, (uint8_t*) msg, strlen(msg));
+        COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler);
+        registered = true;
+
+        // wait for the timer to expire
+        PROCESS_WAIT_UNTIL(etimer_expired(&wait_registration));
+        if(registered){
+            if(etimer_expired(&et)){
+                leds_toggle(LEDS_GREEN);
+                etimer_restart(&et);
+            }
+        }
+    }
+    LOG_INFO("REGISTERED\nStarting alert server");
+
+    // RESOURCES ACTIVATION
+    coap_activate_resource(&alert_actuator, "alert_actuator");
+    coap_activate_resource(&alert_switch_actuator, "alert_switch_actuator");
+
+    // SIMULATION
+    etimer_set(&simulation, CLOCK_SECOND * SIMULATION_INTERVAL);
+    LOG_INFO("Simulation\n");
+
+    while (1) {
+        PROCESS_WAIT_EVENT();
+
+        if (ev == PROCESS_EVENT_TIMER && data == &simulation) {
+            alert_actuator.trigger();
+            etimer_set(&simulation, CLOCK_SECOND * SIMULATION_INTERVAL);
+        }
+
+        if ((ev == button_hal_press_event) && !pressed) {
+            //registered la variabile per capire se si è registrata al border router
+            if (registered) {
+                printf("Button pressed\n");
+                btn = (button_hal_button_t *)data;
+                printf("Release event (%s)\n", BUTTON_HAL_GET_DESCRIPTION(btn));
+                pressed = !pressed;
+                etimer_set(&timeout_timer,TIMEOUT_INTERVAL*CLOCK_SECOND);
+            }
+        }
+        if (pressed) {
+            if (registered) {
+                // Client has put info, stop the running timer (if any)
+                etimer_stop(&timeout_timer);
+                printf("Running timer stopped!\n");
+                alert_switch_actuator.trigger();
+                pressed = false;
+
+            }
+        }
+
+    }
+
+    PROCESS_END();
+}
---
+++ sensors/coap_sensor_alert/resources/res_alert.c
@@ -0,0 +1,98 @@
+#include "contiki.h"
+#include "coap-engine.h"
+#include <string.h>
+#include "time.h"
+#include "os/dev/leds.h"
+#include "sys/etimer.h"
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "alert actuator"
+#define LOG_LEVEL LOG_LEVEL_DBG
+
+static bool isActive = true;
+static int intensity = 10;
+
+static void get_intensity_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void post_switch_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_event_handler(void);
+
+
+//qui costruisco la response che devo dare al client
+
+EVENT_RESOURCE(alert_actuator, //--> name
+"title=\"alert sensor: ?obs",   //---> descriptor (obs significa che è osservabile)
+get_intensity_handler,
+post_switch_handler,
+NULL,
+NULL,
+res_event_handler); //--> handler invoke auto  every time the state of resource change
+
+//get per sapere lo stato
+static void get_intensity_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+    // Create a JSON message with the detected presence value and led value
+    // In both the resources the get_handler return the current sensor values
+    int length;
+
+    char msg[300];
+    // T = true
+    // N = negative
+    char val2 = isActive == 1 ? 'T': 'N';
+    strcpy(msg,"{\"info\":\"");
+    strncat(msg,&val2,1);
+    strcat(msg,"\" ");
+    char* intensity_str = "";
+    sprintf(intensity_str, "%d", intensity);
+    strncat(msg,intensity_str,1);
+    strcat(msg,"\"}");
+    length = strlen(msg);
+    memcpy(buffer, (uint8_t *)msg, length);
+
+    coap_set_header_content_format(response, TEXT_PLAIN);
+    coap_set_header_etag(response, (uint8_t *)&length, 1);
+    coap_set_payload(response, (uint8_t *)buffer, length);
+
+}
+
+
+//usata per fare on/off
+static void post_switch_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+    LOG_INFO("Handling switch post request...\n");
+
+    size_t len = 0;
+    const uint8_t* payload = NULL;
+    bool success = true;
+
+    if((len = coap_get_payload(request, &payload)))
+    {
+        if (strncmp((char*)payload, "ON", strlen("ON")) == 0)
+        {
+            isActive = true;
+            //ogni volta che viene chiamata di seguito una ON intensifica di 10 la potenza
+            if(intensity<100){
+                intensity=intensity+10;
+            }
+            LOG_INFO("Switch on\n");
+        }
+        if (strncmp((char*)payload, "OFF", strlen("OFF")) == 0)
+        {
+            isActive = false;
+            //reset dell'intensita appena viene spento così riparte da 10
+            intensity=10;
+            LOG_INFO("Switch off\n");
+        }
+    } else
+        success = false;
+
+    if(!success)
+        coap_set_status_code(response, BAD_REQUEST_4_00);
+}
+
+
+static void res_event_handler(void)
+{
+    if (isActive)
+        coap_notify_observers(&alert_actuator);
+}
