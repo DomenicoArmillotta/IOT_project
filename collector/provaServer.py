7a53fc70019180f35b4baaadf63edc8763e0ec57
Domenico <10582521@polimi.it>
2022-07-17 21:56:53 +0200

    added serverProva

---
+++ collector/provaServer.py
@@ -0,0 +1,36 @@
+# Import package
+import paho.mqtt.client as mqtt
+
+# Define Variables
+MQTT_HOST = "localhost"
+MQTT_PORT = 1883
+MQTT_KEEPALIVE_INTERVAL = 45
+MQTT_TOPIC = "info"
+MQTT_MSG = "hello MQTT"
+
+# Define on connect event function
+# We shall subscribe to our Topic in this function
+def on_connect(mosq, obj, rc):
+    mqttc.subscribe(MQTT_TOPIC, 0)
+
+# Define on_message event function.
+# This function will be invoked every time,
+# a new message arrives for the subscribed topic
+def on_message(mosq, obj, msg):
+    print ("Topic: " + str(msg.topic))
+    print ("QoS: " + str(msg.qos))
+    print ("Payload: " + str(msg.payload))
+
+
+# Initiate MQTT Client
+mqttc = mqtt.Client()
+
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+
+# Connect with MQTT Broker
+mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
+
+# Continue monitoring the incoming messages for subscribed topic
+mqttc.loop_forever()
